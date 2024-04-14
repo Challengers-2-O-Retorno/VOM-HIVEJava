99028424d8baa76a3a6900a3dbfac3ee07ee30e9
Mmateus106 <rm98524@fiap.com.br>
2024-04-14 15:30:11 -0300

    ProductResponse finalizado

--- src/main/java/com/example/VOM_HiveJava/dto/response/ProductResponse.java
+++ src/main/java/com/example/VOM_HiveJava/dto/response/ProductResponse.java
@@ -1,4 +1,9 @@
 package com.example.VOM_HiveJava.dto.response;
 
-public record ProductResponse() {
+public record ProductResponse(
+
+        int id_product,
+        String nm_product,
+        String category
+) {
 }
